df8841d20e98ebd706ab73de6159d958a091e608
ramansaini14 <ramandeepsinghsaini55@gmail.com>
2026-06-05 19:55:17 +0530

    first commit

---
+++ _next/static/7paHK6oXxNT6RR_lQXuoP/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/medimatch-frontendbuild//_next/:path+",
+        "destination": "/medimatch-frontendbuild/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
